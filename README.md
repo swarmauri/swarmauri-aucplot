27daca2fb61333abb4d46608aa14b4cb3b41dbd0
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-09-03 06:51:29 +0000

    init: gradio auc component

---
+++ README.md
@@ -0,0 +1,12 @@
+# Development Procedure
+
+My Development Procedure for building a Custom gradle component X-Y chart.
+
+1. Installing Gradle package
+
+`pip install gradle`
+
+2. I started by setting up a gradle block with a text inside it just to initialize gradle app.
+
+![1](./.github/1.png)
+![2](./.github/2.png)
---
+++ aucplot/demo/app.py
@@ -0,0 +1,22 @@
+import gradio as gr
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve, auc
+
+def plot_auc(y_true, y_scores):
+    fpr, tpr, _ = roc_curve(y_true, y_scores)
+    roc_auc = auc(fpr, tpr)
+    
+    plt.figure()
+    plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver Operating Characteristic')
+    plt.legend(loc="lower right")
+    plt.savefig('/tmp/auc_plot.png')
+    
+    return '/tmp/auc_plot.png'
+
+gr.Interface(plot_auc, inputs=["text", "text"], outputs="image").launch()
